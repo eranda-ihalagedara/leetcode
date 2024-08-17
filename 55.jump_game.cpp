73aa835e54fbd5027f1d376efd65293e40d4f1ba
Eranda Ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2024-08-17 16:16:10 +1000

    CPP 55

---
+++ 55.jump_game.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n = nums.size()-1;
+        for(int i=n; i >= 0; --i){
+            if(i+nums[i] >= n)
+                n = i;
+        }
+
+        return n == 0;
+
+    }
+};
